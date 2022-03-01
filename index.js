c6c39d4b706fb81a6bf24175b8471dc6041d2829
bilalozair <bo569@nyu.edu>
2022-03-01 19:22:07 +0500

    commit tree 12dddde7729b14e255dc3c669a958c27ac007793

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distB) {
+    //returns the number of blocks given a value
+    return Math.abs(42 - distB);
+}
+function distanceFromHqInFeet(distB) {
+    let distF = 264 * distanceFromHqInBlocks(distB);
+    return distF;
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((destination - start)*264);
+}
+function calculatesFarePrice(start, destination) {
+    let travelF = distanceTravelledInFeet(start,destination);
+    let rate = 0.02;
+    let farePrice;
+    if (travelF <= 400) {
+        return farePrice = 0;
+    } else if (travelF <= 2000) {
+        return farePrice = rate*(travelF-400);
+    } else if (travelF <= 2500) {
+        return farePrice = 25;
+    } else {
+        return farePrice = 'cannot travel that far';
+    }  
+}
